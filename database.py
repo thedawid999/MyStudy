9aa35aab663659911aefe834c2d1aded1821b16a
Dawidek <dawidjedlinski4@gmail.com>
2025-03-11 16:28:16 +0100

    add all methods to database

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 import sqlite3
 from course import Course
+from time_goal import TimeGoal
+from value_goal import ValueGoal
+
 
 # noinspection SqlNoDataSourceInspection
 class Database:
@@ -11,7 +14,7 @@ class Database:
     def create_tables(self):
         """creates tables for courses, value goals and time goals"""
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS timegoal (
+            CREATE TABLE IF NOT EXISTS timegoals (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title TEXT NOT NULL,
                 startdate TEXT NOT NULL,
@@ -20,7 +23,7 @@ class Database:
         ''')
 
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS valuegoal (
+            CREATE TABLE IF NOT EXISTS valuegoals (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title TEXT NOT NULL,
                 value REAL NOT NULL,
@@ -28,7 +31,7 @@ class Database:
         ''')
 
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS course (
+            CREATE TABLE IF NOT EXISTS courses (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL,
                 grade REAL DEFAULT 0,
@@ -39,27 +42,55 @@ class Database:
     def add_course(self, course:Course):
         """adds a course to the database, if a grade for this course exists it will be added too"""
         if course.grade is not 0:
-            self.cursor.execute("INSERT INTO course (name, grade) VALUES (?, ?)", (course.name, course.grade))
+            self.cursor.execute("INSERT INTO courses (name, grade) VALUES (?, ?)", (course.name, course.grade))
         else:
-            self.cursor.execute("INSERT INTO course (name) VALUES (?)", course.name)
+            self.cursor.execute("INSERT INTO courses (name) VALUES (?)", course.name)
         self.conn.commit()
 
     def delete_course(self, course:Course):
         """deletes a course from the database"""
-        self.cursor.execute("DELETE FROM course WHERE name = ?", course.name)
+        self.cursor.execute("DELETE FROM courses WHERE name = ?", course.name)
         self.conn.commit()
 
     def add_grade(self, course:Course):
         """adds a grade to its course"""
-        self.cursor.execute("UPDATE course SET grade = ? WHERE name = ?", (course.grade, course.name))
+        self.cursor.execute("UPDATE courses SET grade = ? WHERE name = ?", (course.grade, course.name))
+        self.conn.commit()
 
     def delete_grade(self, course:Course):
         """deletes a grade from the database"""
-        self.cursor.execute("UPDATE course SET grade = 0 WHERE name = ?", course.name)
+        self.cursor.execute("UPDATE courses SET grade = 0 WHERE name = ?", course.name)
+        self.conn.commit()
 
+    def get_courses(self):
+        """returns all courses from database"""
+        self.cursor.execute("SELECT name,grade FROM courses")
+        return self.cursor.fetchall()
 
     #----------------------Methods for TimeGoal Database----------------------
-    ###
+    def add_time_goal(self, timegoal: TimeGoal):
+        self.cursor.execute("INSERT INTO timegoals (title, startdate, deadline) VALUES (?,?,?)",
+                            (timegoal.title, timegoal.startdate, timegoal.deadline))
+        self.conn.commit()
+
+    def delete_time_goal(self, timegoal:TimeGoal):
+        self.cursor.execute("DELETE FROM timegoals WHERE title = ?", timegoal.title)
+        self.conn.commit()
+
+    def get_time_goals(self):
+        self.cursor.execute("SELECT title, startdate, deadline FROM timegoals")
+        return self.cursor.fetchall()
 
     #----------------------Methods for ValueGoal Database----------------------
-    ###
+    def add_value_goal(self, valuegoal:ValueGoal):
+        self.cursor.execute("INSERT INTO valuegoals (title, value) VALUES (?,?)",
+                            (valuegoal.title, valuegoal.value))
+        self.conn.commit()
+
+    def delete_value_goal(self, valuegoal:ValueGoal):
+        self.cursor.execute("DELETE FROM valuegoals WHERE title = ?", valuegoal.title)
+        self.conn.commit()
+
+    def get_value_goals(self):
+        self.cursor.execute("SELECT title, value FROM valuegoals")
+        return self.cursor.fetchall()
