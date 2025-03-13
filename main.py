0fe930141d6d11904c9cfe1c8294f103dbefdc28
Dawidek <dawidjedlinski4@gmail.com>
2025-03-13 17:57:07 +0100

    define calculate_min_grade()

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from reader import Reader
 def main():
     #TODO: finish here
     #TODO: SUBSCRIBE TO EVENTS!!!
+    #TODO: specify that max. 1 valuegoal and max. 3 timegoals can be added
     while True:
         Reader.read_input()
 
--- value_goal.py
+++ value_goal.py
@@ -1,4 +1,5 @@
 from goal import Goal
+from student import Student
 
 class ValueGoal(Goal):
     def __init__(self, title:str, value:float):
@@ -12,11 +13,23 @@ class ValueGoal(Goal):
         return self._value
 
     @staticmethod
-    def calculate_average(self):
-        # TODO: define method!
-        return None
+    def calculate_average(self, student:Student):
+        grades = ValueGoal.__extract_grades(self, student)
+        avg = sum(grades) / len(grades)
+        return avg
 
     @staticmethod
-    def calculate_min_grade(self):
-        # TODO: define method!
-        return None
+    def calculate_min_grade(self, student:Student):
+        grades = ValueGoal.__extract_grades(self, student)
+        courses = student.get_courses()
+
+        minimum = (self._value*len(courses)-sum(grades))/(len(courses)-len(grades))
+        return minimum
+
+    @staticmethod
+    def __extract_grades(self, student:Student):
+        grades = []
+        for course in student.get_courses():
+            if course.grade != 0:
+                grades.append(course.grade)
+        return grades
